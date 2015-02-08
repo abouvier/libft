b51ae5d6463c84ed04e74da00b1fbd1434d4d1ca
abouvier <abouvier@student.42.fr>
2015-02-08 10:16:27 +0100

    add gnl

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,1 +1,31 @@
-../../getnextline/include/get_next_line.h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/19 11:27:01 by abouvier          #+#    #+#             */
+/*   Updated: 2014/01/03 14:55:53 by abouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+# define BUF_SIZE	1024
+
+# define CHUNK		chunk->content
+# define CHUNK_SIZE	chunk->content_size
+
+typedef struct	s_buffer
+{
+	size_t		size;
+	t_list		*chunk;
+}				t_buffer;
+
+int				get_next_line(int const fd, char **line);
+
+#endif
--- src/get_next_line.c
+++ src/get_next_line.c
@@ -1,1 +1,93 @@
-../../getnextline/src/get_next_line.c
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/19 12:42:08 by abouvier          #+#    #+#             */
+/*   Updated: 2014/01/03 15:00:36 by abouvier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static int	reset_buf(t_buffer *buf, char **junk)
+{
+	ft_lstdel(&buf->chunk, &free);
+	if (junk)
+		ft_strdel(junk);
+	buf->size = 0;
+	return (-1);
+}
+
+static void	build_line(t_buffer *buf, char *line)
+{
+	t_list	*next;
+
+	while (buf->chunk)
+	{
+		next = buf->chunk->next;
+		buf->size -= buf->CHUNK_SIZE;
+		ft_memcpy(line + buf->size, buf->CHUNK, buf->CHUNK_SIZE);
+		ft_lstdelone(&buf->chunk, &free);
+		buf->chunk = next;
+	}
+}
+
+static int	set_next_line(t_buffer *buf, char *newline, char **line)
+{
+	char	*s;
+	t_list	*chunk;
+	size_t	new_size;
+
+	if (newline)
+	{
+		new_size = buf->CHUNK_SIZE - (newline - (char *)buf->CHUNK) - 1;
+		if (!(s = ft_strnew(new_size - 1)))
+			return (reset_buf(buf, NULL));
+		ft_memcpy(s, newline + 1, new_size);
+		buf->CHUNK_SIZE -= new_size + 1;
+		buf->size -= new_size + 1;
+	}
+	if (!(*line = ft_strnew(buf->size)))
+		return (reset_buf(buf, &s));
+	build_line(buf, *line);
+	if (newline)
+	{
+		if (!(chunk = ft_lstnew(s, new_size)))
+			return (reset_buf(buf, &s));
+		ft_lstadd(&buf->chunk, chunk);
+		buf->size = new_size;
+	}
+	return (1);
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	ssize_t			r;
+	char			*n;
+	char			*s;
+	static t_buffer	buf;
+	t_list			*chunk;
+
+	r = 42;
+	while (!buf.chunk
+		|| (!(n = ft_memchr(buf.CHUNK, '\n', buf.CHUNK_SIZE)) && r))
+	{
+		if (!(s = ft_strnew(BUF_SIZE - 1))
+			|| (r = read(fd, s, BUF_SIZE)) < 0
+			|| !(chunk = ft_lstnew(s, r)))
+			return (reset_buf(&buf, &s));
+		ft_lstadd(&buf.chunk, chunk);
+		buf.size += r;
+	}
+	if (buf.size)
+		return (set_next_line(&buf, n, line));
+	reset_buf(&buf, NULL);
+	return (0);
+}
