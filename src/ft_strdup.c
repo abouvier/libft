abf1fc3724c4eb31a3322ba307e67a46dfd2d2ba
abouvier <abouvier@student.42.fr>
2015-02-08 10:18:09 +0100

    improve ft_strdup

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,18 +6,20 @@
 /*   By: abouvier <abouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 10:54:22 by abouvier          #+#    #+#             */
-/*   Updated: 2013/11/21 10:54:30 by abouvier         ###   ########.fr       */
+/*   Updated: 2015/01/31 02:28:20 by abouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strdup(const char *s)
 {
+	size_t	len;
 	char	*dup;
 
-	dup = (char *)ft_calloc(ft_strlen(s) + 1, sizeof(*dup));
-	if (dup)
-		ft_strcpy(dup, s);
+	len = ft_strlen(s) + 1;
+	if ((dup = malloc(len)))
+		ft_memcpy(dup, s, len);
 	return (dup);
 }
