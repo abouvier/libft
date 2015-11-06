4788d35f6cd09f16e09796d998fd7155b2bb0a30
abouvier <abouvier@student.42.fr>
2015-11-06 06:31:16 +0100

    fix cast

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (n--)
 	{
 		if (*s1 != *s2)
-			return ((unsigned char)*s1 < (unsigned char)*s2 ? -1 : 1);
+			return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1);
 		else if (!*s1)
 			return (0);
 		s1++;
